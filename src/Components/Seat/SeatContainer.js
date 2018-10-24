825f42aa31c52a54ff5db0a49f8fd107fa7e2420
Bishnu Basyal <imvsnu@gmail.com>
2018-10-25 00:27:32 +0545

    ticket-book - create all functionality for booking tickets

--- src/Components/Seat/SeatContainer.js
+++ src/Components/Seat/SeatContainer.js
@@ -2,16 +2,89 @@ import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import { withFormik } from "formik";
 import { connect } from "react-redux";
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import TicketComponent from "../Ticket/TicketComponent";
 import * as Yup from "yup";
 import "./Seat.css";
 
 class SeatMap extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      selectedSeats: [],
+      totalAmount  : 100,
+      showModal    : false
+    };
+  }
+
+  toggleSeatSelect = (seatNumber) => {
+    if(this.state.selectedSeats.includes(seatNumber)) {
+      const selectedSeats = [...this.state.selectedSeats];
+      const index = selectedSeats.indexOf(seatNumber);
+      selectedSeats.splice(index, 1);
+      this.setState({ 
+        selectedSeats 
+      });
+    }
+    else
+      this.setState({ 
+        selectedSeats: [...this.state.selectedSeats, seatNumber] 
+      });
+  }
+
+  showModal = () => {
+    this.setState({ showModal: true });
+  }
+
+  hideModal = () => {
+    this.setState({ showModal: false });
+  }
+
+  print = () => {
+    console.log('Print');
+    var content = document.getElementById('ticket');
+    var pri = document.getElementById('ifmcontentstoprint').contentWindow;
+    pri.document.open();
+    pri.document.write(content.innerHTML);
+    pri.document.close();
+    pri.focus();
+    pri.print();
   }
 
   render() {
+    const { selectedSeats, totalAmount, showModal } = this.state;
+    const seatNumbers = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9'];
+    const seatPlacement = seatNumbers.map(seatNumber => {
+      return <div key={seatNumber} 
+                  className={selectedSeats.includes(seatNumber)? 'seat selected' : 'seat'} 
+                  onClick={() => this.toggleSeatSelect(seatNumber)}>
+                  {seatNumber}
+             </div>
+    });
+    const selectedSeatsDetail = <table class="Displaytable">
+        <tr>
+          <th>Name</th>
+          <th>Number of Seats</th>
+          <th>Seats</th>
+        </tr>
+        <tr key={selectedSeats.length}>
+          <td>
+            <textarea id="nameDisplay" disabled>
+              Ram
+            </textarea>
+          </td>
+          <td>
+            <textarea id="NumberDisplay" disabled>
+              {selectedSeats.length}
+            </textarea>
+          </td>
+          <td>
+            <textarea id="seatsDisplay" disabled>
+              {selectedSeats.length > 0 ? selectedSeats.toString() : 'No seat selected'}
+            </textarea>
+          </td>
+        </tr>
+      </table>
     return (
       <div
       style={{
@@ -25,6 +98,7 @@ class SeatMap extends Component {
             <img
               class="cover"
               src="https://image.tmdb.org/t/p/w300/gfJGlDaHuWimErCr5Ql0I8x9QSy.jpg"
+              alt='movie'
               style={{
                 backgroundImage:
                   "url(https://image.tmdb.org/t/p/w300//gfJGlDaHuWimErCr5Ql0I8x9QSy.jpg)"
@@ -45,7 +119,10 @@ class SeatMap extends Component {
             </ul>
             <span>Select your seats</span>
             <div class="seats">
-              <div class="seat taken ">A1</div>
+              {
+                seatPlacement
+              }
+              {/* <div class="seat taken ">A1</div>
               <div class="seat  aisle-right">A2</div>
               <div class="seat  ">A3</div>
               <div class="seat  ">A4</div>
@@ -122,37 +199,14 @@ class SeatMap extends Component {
               <div class="seat  aisle-top">I3</div>
               <div class="seat  aisle-top">I4</div>
               <div class="seat  aisle-top">I5</div>
-              <div class="seat taken aisle-top">I6</div>
+              <div class="seat taken aisle-top">I6</div> */}
             </div>
             <div class="screen">screen</div>
             <br />
             <br />
             <div class="displayerBoxes">
               <center>
-                <table class="Displaytable">
-                  <tr>
-                    <th>Name</th>
-                    <th>Number of Seats</th>
-                    <th>Seats</th>
-                  </tr>
-                  <tr>
-                    <td>
-                      <textarea id="nameDisplay" disabled>
-                        Ram
-                      </textarea>
-                    </td>
-                    <td>
-                      <textarea id="NumberDisplay" disabled>
-                        3
-                      </textarea>
-                    </td>
-                    <td>
-                      <textarea id="seatsDisplay" disabled>
-                        A8, A9, A10
-                      </textarea>
-                    </td>
-                  </tr>
-                </table>
+                {selectedSeatsDetail}
               </center>
             </div>
             <div
@@ -160,10 +214,42 @@ class SeatMap extends Component {
                 marginTop: "50px"
               }}
             >
-              <small>Total </small>
-              <span>$21.76</span>
+              <small>Total Paying Amount: </small>
+              <span>Rs.{totalAmount * selectedSeats.length}</span>
               &nbsp;&nbsp;&nbsp;
-              <Button variant="contained" color="primary" type="button">
+              <Modal isOpen={showModal} size="lg">
+                <ModalHeader toggle={this.hideModal}>CHECKOUT</ModalHeader>
+                <iframe
+                  title='ticket' 
+                  id="ifmcontentstoprint" style={{
+                  height: '0px',
+                  width: '0px',
+                  position: 'absolute'
+                }}></iframe> 
+                <ModalBody id='ticket'>
+                    <TicketComponent
+                    totalAmount={totalAmount * selectedSeats.length}
+                    selectedSeats={selectedSeats}/>
+                </ModalBody>
+                <ModalFooter>
+                  <Button 
+                    color="secondary" 
+                    onClick={this.hideModal}>
+                    Cancel
+                  </Button>
+                  <Button 
+                    color="primary"
+                    onClick={this.print}>
+                    Print
+                  </Button>
+                 </ModalFooter>
+              </Modal>
+              <Button 
+                variant="contained" 
+                color="primary" 
+                type="button" 
+                onClick={this.showModal}
+                disabled={selectedSeats.length < 1} >
                 CHECKOUT
               </Button>
               <div class="loader" />
